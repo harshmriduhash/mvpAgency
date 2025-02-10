5d2bf2be3fa0dfdc890931da10842fff5982ccef
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-02-10 12:11:52 +0530

    Marquee

--- components/landing-ui/hero/marquee.tsx
+++ components/landing-ui/hero/marquee.tsx
@@ -1,11 +1,11 @@
-import * as React from "react"
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
 interface MarqueeProps extends React.HTMLAttributes<HTMLDivElement> {
-  children: React.ReactNode
-  pauseOnHover?: boolean
-  direction?: "left" | "right"
-  speed?: number
+  children: React.ReactNode;
+  pauseOnHover?: boolean;
+  direction?: "left" | "right";
+  speed?: number;
 }
 
 export function Marquee({
@@ -17,15 +17,12 @@ export function Marquee({
   ...props
 }: MarqueeProps) {
   return (
-    <div 
-      className={cn(
-        "w-full overflow-hidden sm:mt-24 mt-10 z-10",
-        className
-      )} 
+    <div
+      className={cn("w-full overflow-hidden sm:mt-24 mt-10 z-10", className)}
       {...props}
     >
       <div className="relative flex max-w-[90vw] overflow-hidden py-5">
-        <div 
+        <div
           className={cn(
             "flex w-max animate-marquee",
             pauseOnHover && "hover:[animation-play-state:paused]",
@@ -38,5 +35,5 @@ export function Marquee({
         </div>
       </div>
     </div>
-  )
+  );
 }
