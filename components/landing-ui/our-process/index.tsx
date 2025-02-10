0a91f2cfdc4af70710add61e354fefbcba864cc8
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-02-10 12:10:51 +0530

    Process

--- components/landing-ui/our-process/index.tsx
+++ components/landing-ui/our-process/index.tsx
@@ -16,31 +16,46 @@ function OurProcess() {
                 <span className="absolute flex items-center justify-center w-8 h-8 bg-[#2c5688] rounded-full -start-4 ring-4 ring-white dark:ring-gray-900 dark:bg-[#2c5688]">
                   <FileText className="h-5 w-5 text-white" />
                 </span>
-                <h3 className="font-medium leading-tight text-primary text-xl">Specification</h3>
-                <p className="text-sm">We define your MVP goals with a clear specifications.</p>
+                <h3 className="font-medium leading-tight text-primary text-xl">
+                  Specification
+                </h3>
+                <p className="text-sm">
+                  We define your MVP goals with a clear specifications.
+                </p>
               </li>
               <li className="mb-10 ms-7">
                 <span className="absolute flex items-center justify-center w-8 h-8 bg-[#2c5688] rounded-full -start-4 ring-4 ring-white dark:ring-gray-900 dark:bg-[#2c5688]">
                   <BellRing className="h-5 w-5 text-white" />
                 </span>
-                <h3 className="font-medium leading-tight text-primary text-xl">Development</h3>
-                <p className="text-sm">We bring your MVP to life with regular updates.</p>
+                <h3 className="font-medium leading-tight text-primary text-xl">
+                  Development
+                </h3>
+                <p className="text-sm">
+                  We bring your MVP to life with regular updates.
+                </p>
               </li>
               <li className="mb-10 ms-7">
                 <span className="absolute flex items-center justify-center w-8 h-8 bg-[#2c5688] rounded-full -start-4 ring-4 ring-white dark:ring-gray-900 dark:bg-[#2c5688]">
                   <PackageCheck className="h-5 w-5 text-white" />
                 </span>
-                <h3 className="font-medium leading-tight text-primary text-xl">Confirmation & Review</h3>
+                <h3 className="font-medium leading-tight text-primary text-xl">
+                  Confirmation & Review
+                </h3>
                 <p className="text-sm">
-                  We finalize your MVP based on feedback and ensure everything is ready for launch.
+                  We finalize your MVP based on feedback and ensure everything
+                  is ready for launch.
                 </p>
               </li>
               <li className="ms-7">
                 <span className="absolute flex items-center justify-center w-8 h-8 bg-[#2c5688] rounded-full -start-4 ring-4 ring-white dark:ring-gray-900 dark:bg-[#2c5688]">
                   <Rocket className="h-5 w-5 text-white" />
                 </span>
-                <h3 className="font-medium leading-tight text-primary text-xl">Launch</h3>
-                <p className="text-sm">We deploy and guide you through managing your MVP.</p>
+                <h3 className="font-medium leading-tight text-primary text-xl">
+                  Launch
+                </h3>
+                <p className="text-sm">
+                  We deploy and guide you through managing your MVP.
+                </p>
               </li>
             </ol>
           </div>
