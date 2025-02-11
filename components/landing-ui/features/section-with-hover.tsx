621244e15cd79fa4b558f346c4c2bba4f989c0ea
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-02-11 11:50:42 +0530

    HOVER SECTION

--- components/landing-ui/features/section-with-hover.tsx
+++ components/landing-ui/features/section-with-hover.tsx
@@ -1,12 +1,20 @@
 import { cn } from "@/lib/utils";
-import { Activity, Component, GalleryVerticalEnd, Network, SearchCheck, ShieldCheck, Users, Zap } from "lucide-react";
+import {
+  Activity,
+  Component,
+  GalleryVerticalEnd,
+  Network,
+  SearchCheck,
+  ShieldCheck,
+  Users,
+  Zap,
+} from "lucide-react";
 
 export function SectionWithHover() {
   const features = [
     {
       title: "Rapid Delivery",
-      description:
-        "Launch your MVP in just 2-4 weeks.",
+      description: "Launch your MVP in just 2-4 weeks.",
       icon: <Zap />,
     },
     {
@@ -41,12 +49,14 @@ export function SectionWithHover() {
     },
     {
       title: "Reliable Uptime",
-      description: "Guaranteed 100% uptime for uninterrupted access to your MVP.",
+      description:
+        "Guaranteed 100% uptime for uninterrupted access to your MVP.",
       icon: <Activity />,
     },
     {
       title: "Proven Expertise",
-      description: "With years of experience, we know how to get your idea to market quickly and effectively.",
+      description:
+        "With years of experience, we know how to get your idea to market quickly and effectively.",
       icon: <Users />,
     },
   ];
@@ -85,9 +95,7 @@ const Feature = ({
       {index >= 4 && (
         <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none" />
       )}
-      <div className="mb-4 relative z-10 px-10 text-[#2c5688]">
-        {icon}
-      </div>
+      <div className="mb-4 relative z-10 px-10 text-[#2c5688]">{icon}</div>
       <div className="text-lg font-bold mb-2 relative z-10 px-10">
         <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-[#2c5688] transition-all duration-200 origin-center" />
         <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-neutral-800 dark:text-neutral-100">
